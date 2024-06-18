8d24aa0cd9572d0062cbc3862932dd899e006f61
flammarionick <n.eke@alustudent.com>
2024-06-18 19:07:44 +0000

    sui

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-# web static development
-
+# Install Nginx if not already installed
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
+
+# create folders
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+#create a fake html file
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+# create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# give ownership
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
-from os.path import exists
-from fabric.api import env, put, run
+""" Function that compress a folder """
 from datetime import datetime
+from fabric.api import *
+import shlex
+import os
 
-# Setup Fabric environment variables.
-env.hosts = ['54.90.67.236', '54.90.237.122']
-env.user = 'ubuntu'
+
+env.hosts = ['54.235.14.97', '54.205.221.93']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-    """
-    if exists(archive_path) is False:
+    """ The fuction doing Deployments """
+    if not os.path.exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        file_p = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}{}/".format(path, file_p))
-        run("sudo tar -xzf /tmp/{} -C {}{}/".format(file_n, path, file_p))
-        run("sudo rm /tmp/{}".format(file_n))
-        run("sudo mv -n {0}{1}/web_static/* {0}{1}/".format(path, file_p))
-        run("sudo rm -rf {}{}/web_static".format(path, file_p))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {}{}/ /data/web_static/current".format(path, file_p))
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
     except:
         return False
